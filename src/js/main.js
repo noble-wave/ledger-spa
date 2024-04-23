dba7a2a9b2ac01f941821c38b625f29025477fa3
Nitish-Repo <nitishkumar987654@gmail.com>
2024-04-23 17:07:34 +0530

    inventoryManagement html add and work on blog page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import * as bootstrap from 'bootstrap';
+
 const demo = () => 'Webpack Boilerplate v5.18.0 - SASS/PostCSS, ES6/7, browser sync.';
 
 // eslint-disable-next-line no-console
@@ -27,17 +29,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // eslint-disable-next-line vars-on-top
-var scrollTimeout;
+// var scrollTimeout;
 
-window.onscroll = function () {
-  clearTimeout(scrollTimeout);
-  scrollTimeout = setTimeout(scrollFunction, 100); // Adjust the debounce delay as needed
-};
+// window.onscroll = function () {
+//   clearTimeout(scrollTimeout);
+//   scrollTimeout = setTimeout(scrollFunction, 100); // Adjust the debounce delay as needed
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 100 || window.innerWidth <= 992) {
-    document.getElementById('scrollspyNav').style.padding = '20px 10px';
-  } else {
-    document.getElementById('scrollspyNav').style.padding = '50px 10px';
-  }
-}
+// // function scrollFunction() {
+// //   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 100 || window.innerWidth <= 992) {
+// //     document.getElementById('scrollspyNav').style.padding = '20px 10px';
+// //   } else {
+// //     document.getElementById('scrollspyNav').style.padding = '50px 10px';
+// //   }
+// // }
