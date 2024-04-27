40c36cc3be3cd41768d02422c3dbe228331e795f
Nitish-Repo <nitishkumar987654@gmail.com>
2024-04-27 10:22:27 +0530

    our bolg css fix and also service apge add some content also

--- src/js/main.js
+++ src/js/main.js
@@ -5,27 +5,32 @@ const demo = () => 'Webpack Boilerplate v5.18.0 - SASS/PostCSS, ES6/7, browser s
 // eslint-disable-next-line no-console
 console.log(demo());
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('navigateLedgerApp').addEventListener('click', function () {
-    window.location.href = 'https://my-ledger.etrivia.app/';
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('learnMore').addEventListener('click', function () {
-    const sectionId = 'scrollspyFeatures';
-    const sectionPosition = document.getElementById(sectionId).offsetTop;
-    window.scrollTo({
-      top: sectionPosition,
-      behavior: 'smooth',
-    });
-  });
-});
-
 const getCurrentYear = () => new Date().getFullYear();
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('currentYear').textContent = getCurrentYear();
+  const navigateLedgerApp = document.getElementById('navigateLedgerApp');
+  if (navigateLedgerApp) {
+    navigateLedgerApp.addEventListener('click', function () {
+      window.location.href = 'https://my-ledger.etrivia.app/';
+    });
+  }
+
+  const learnMore = document.getElementById('learnMore');
+  if (learnMore) {
+    learnMore.addEventListener('click', function () {
+      const sectionId = 'scrollspyFeatures';
+      const sectionPosition = document.getElementById(sectionId).offsetTop;
+      window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth',
+      });
+    });
+  }
+
+  const currentYear = document.getElementById('currentYear');
+  if (currentYear) {
+    currentYear.textContent = getCurrentYear();
+  }
 });
 
 // eslint-disable-next-line vars-on-top
